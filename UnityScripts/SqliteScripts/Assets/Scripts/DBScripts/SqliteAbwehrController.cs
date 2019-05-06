832c4bc894012d69c135c7c50e1662d403b133a3
BenKato <benjamin.kiel@edu.bib.de>
2019-05-06 14:53:19 +0200

    Added single table import into a new db
    look up TODO.txt

--- UnityScripts/SqliteScripts/Assets/Scripts/DBScripts/SqliteAbwehrController.cs
+++ UnityScripts/SqliteScripts/Assets/Scripts/DBScripts/SqliteAbwehrController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Xml;
 using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.UI;
@@ -237,13 +238,57 @@ namespace SqliteAbwehrController
         }
         #endregion
 
+        
         #region ImportXML
         public void ImportXML()
         {
-            //Unterricht
-            XDocument abwehrtableFile = XDocument.Load(Application.dataPath + @"/XMLDocuments/Imports/gurbslight_character_export.xml");
-            SqliteConnection.CreateFile(Application.dataPath + @"/Scripts/Database/new.sqlite");
-            
+                //Unterricht
+            try
+            {
+                string dbpath = Application.dataPath + @"/Scripts/Database/new.sqlite";
+                string xmlpath = Application.dataPath + @"/XMLDocuments/Imports/gurbslight_character_export.xml";
+                XmlDocument abwehrtableFile = new XmlDocument();
+                abwehrtableFile.Load(xmlpath);
+                //XmlDocument document = new XmlDocument().LoadXml(abwehrtableFile);
+                XmlNode selectabwehrschild =  abwehrtableFile.SelectNodes("/GurpsLightCharacter/Abwehr")[0].ChildNodes[0];
+                XmlNode selectabwehrruestung = abwehrtableFile.SelectNodes("/GurpsLightCharacter/Abwehr")[0].ChildNodes[1];
+                XmlNode selectabwehrumhang = abwehrtableFile.SelectNodes("/GurpsLightCharacter/Abwehr")[0].ChildNodes[2];
+                XmlNode selectabwehrgesamt = abwehrtableFile.SelectNodes("/GurpsLightCharacter/Abwehr")[0].ChildNodes[3];
+                XmlNode selectabwehrid = abwehrtableFile.SelectNodes("/GurpsLightCharacter/Abwehr")[0].ChildNodes[4];
+
+
+                SqliteConnection.CreateFile(dbpath);
+                SqliteConnection dbconnect = new SqliteConnection("Data Source = " + dbpath + "; " + " Version = 3;");
+                if (File.Exists(dbpath))
+                {
+                    if (dbconnect != null)
+                    {
+                        dbconnect.Open();
+                        string createtable = "CREATE TABLE Abwehr(Schild int, ruestung int, umhang int, gesamt int , ID int);";
+                        SqliteCommand commandCreateTable = new SqliteCommand(createtable,dbconnect);
+                        commandCreateTable.ExecuteNonQuery();
+
+                        string insertinto = "INSERT INTO Abwehr(Schild, ruestung, umhang, gesamt, ID) VALUES(@schild, @ruestung, @umhang, @gesamt, @id);";
+                        SqliteCommand commandinsert = new SqliteCommand(insertinto, dbconnect);
+                        commandinsert.Parameters.Add("@schild", System.Data.DbType.Int32).Value = selectabwehrschild.InnerText;
+                        commandinsert.Parameters.Add("@ruestung", System.Data.DbType.Int32).Value = selectabwehrruestung.InnerText;
+                        commandinsert.Parameters.Add("@umhang", System.Data.DbType.Int32).Value = selectabwehrumhang.InnerText;
+                        commandinsert.Parameters.Add("@gesamt", System.Data.DbType.Int32).Value = selectabwehrgesamt.InnerText;
+                        commandinsert.Parameters.Add("@id", System.Data.DbType.Int32).Value = selectabwehrid.InnerText;
+
+                        commandinsert.ExecuteNonQuery();
+                        commandinsert.Parameters.Clear();
+
+                        Debug.Log("Success!");
+                       
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+
 
 
 
