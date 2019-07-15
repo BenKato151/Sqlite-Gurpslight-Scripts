b0d1bfc949dabc6f273c3868c83de8e7380c3905
BenKato <benjamin.kiel@edu.bib.de>
2019-07-15 17:02:25 +0200

    configured project

--- UnityScripts/SqliteScripts/Assets/Scripts/DBScripts/ImportAll.cs
+++ UnityScripts/SqliteScripts/Assets/Scripts/DBScripts/ImportAll.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Xml;
-using System.Xml.Linq;
-using System.Xml.Xsl;
-using System.Xml.Serialization;
 using System.Collections.Generic;
 using UnityEngine;
 using Mono.Data.Sqlite;
@@ -21,13 +18,11 @@ public class ImportAll : MonoBehaviour {
 
             if (File.Exists(dbpath) && File.Exists(xmlpath))
             {
-                #region XML
                 XmlDocument xmlFile = new XmlDocument();
                 xmlFile.Load(xmlpath);                
 
                 XmlNodeList selectall = xmlFile.SelectNodes("/Characterbogen");
-
-                #endregion
+                
                 #region SQLite
                 SqliteConnection dbconnection = new SqliteConnection("Data Source = " + dbpath + "; " + " Version = 3;");
                 if (dbconnection != null)
@@ -41,20 +36,19 @@ public class ImportAll : MonoBehaviour {
                             string table = node.ChildNodes[i].Name.ToString();
                             List<string> column = new List<string>();
                             List<string> value = new List<string>();
+
                             for (int k = 0; k < node.ChildNodes[i].ChildNodes.Count; k++)
                             {
                                 column.Add(node.ChildNodes[i].ChildNodes[k].Name.ToString());
                                 value.Add(node.ChildNodes[i].ChildNodes[k].InnerText.ToString());
                             }
 
-                            string commandstring = "INSERT INTO " + table + " (" + string.Join(", ",column.ToArray()) + ") VALUES('" + string.Join(", ", value.ToArray())+ "')";
+                            string commandstring = "INSERT INTO " + table + " (" + string.Join(", ",column.ToArray()) + ") VALUES('" + string.Join("', '", value.ToArray())+ "')";
                             SqliteCommand command = new SqliteCommand(commandstring, dbconnection);
                             command.ExecuteNonQuery();
                         }
                     }
-
                     Debug.Log("Success");
-
                 }
                 #endregion
             }
